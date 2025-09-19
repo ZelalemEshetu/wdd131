c5ede0be356ec8f761abae83f5bd26be7137eaf5
ZelalemEshetu <zbesha@byupathway.edu>
2025-09-19 14:14:57 +0300

    ...

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// getdates.js
+
+// Get current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date of the document
+document.getElementById("lastModified").textContent =
+  "Last modified: " + document.lastModified;
