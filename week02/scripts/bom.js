ddfc792ecdb433580d5c6ba047d11d4025f64d25
ZelalemEshetu <zbesha@byupathway.edu>
2025-09-22 11:00:11 +0300

    Add BOM Top 10 app functionality

---
+++ week02/scripts/bom.js
@@ -0,0 +1,32 @@
+/*Create a click event listener for the Add Chapter */
+button.addEventListener('click', function() {
+  // Code to execute when the button is clicked
+});
+ // Step 1: Check if input is empty
+   if (input.value.trim() === "") {
+    alert("Please enter a chapter first!");
+    input.focus(); // put cursor back
+    return; // stop here
+
+    
+  // Step 2: Create a list item
+  const li = document.createElement('li');
+  li.textContent = input.value;
+
+    // Step 3: Create a delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = "❌";
+
+    // Step 4: Add delete function
+  deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+  });
+
+    // Step 5: Attach button to li, then li to ul
+  li.appendChild(deleteButton);
+  list.appendChild(li);
+
+    // Step 6: Clear input and refocus
+  input.value = "";
+  input.focus();
+});
